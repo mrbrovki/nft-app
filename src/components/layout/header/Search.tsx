7babe24a0c158097d92b94e27a447f66b9df0d2a
Mahkamjon Raupov <devbrovki@gmail.com>
2022-07-09 22:53:28 +0200

    header added

--- src/components/layout/header/Search.tsx
+++ src/components/layout/header/Search.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import styled from 'styled-components';
+import { breakpoints } from '../../../lib/constants';
 
+const StyledSearch = styled.div`
+  position: relative;
+  grid-area: 1 / 2 / 2 / 3;
 
+  @media screen and (max-width: ${breakpoints.md}){
+    grid-area: 2 / 1 / 3 / 4;
+  }
+`;
+
+const SearchIcon = styled.img.attrs({alt:'search icon'})` 
+  position: absolute;
+  left: 1rem;
+  top: calc(1rem - 10px);
+`;
+
+const Input = styled.input.attrs({type:'search', size:30})`
+  border: 1px solid #ababab;
+  height: 2rem;
+  border-radius: 1.5rem;
+  width: 100%;
+  padding-left: 3rem;
+  font-size: 1rem;
+  font-weight: 600;
+
+  &::-webkit-search-cancel-button{
+   display: none;
+  }
+
+  &:focus{ 
+    outline: 1px solid #aaaaaa;
+  }
+`;
 
 const Search = () => {
   return (
-    <></>
+    <StyledSearch>
+      <SearchIcon src='/assets/search-icon.svg' alt='search icon'/>
+      <form>
+        <Input placeholder='Search Creation...' />
+      </form>
+    </StyledSearch>
   )
 }
 
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { breakpoints } from '../../../lib/constants';
+import Button from '../../shared/Button';
+import Search from './Search';
 
+const StyledHeader = styled.header`
+  height: 6rem;
+  display: grid;
+  grid-template-columns: 14rem 1fr 8rem 10rem;
+  align-items: center;
+  grid-gap: 1rem;
+  background-color: #fff;
+  position: fixed;
 
+  @media screen and (max-width: ${breakpoints.xl}){
+    padding: 0 10rem;
+    width: calc(100% - 20rem);
+  }
+  @media screen and (max-width: ${breakpoints.lg}){
+    padding: 0 5rem;
+    width: calc(100% - 10rem);
+  }
+  @media screen and (max-width: ${breakpoints.md}){
+    display: none;
+  }
+`;
+
+const Explore = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  &:hover{
+    cursor: pointer;
+  }
+`;
 
 const Header = () => {
   return (
-    <header></header>
+    <StyledHeader>
+      <Link to='/'>
+        <img src='/assets/logo.svg' alt='logo'/>
+      </Link>
+      <Search />
+      <Explore>
+        <span>Explore</span>
+        <img src='/assets/arrow-down-icon-black.svg' alt='arrow down icon'/>
+      </Explore>
+      <Button background='black' color='white'>Connect Wallet</Button>
+    </StyledHeader>
   )
 }
 
